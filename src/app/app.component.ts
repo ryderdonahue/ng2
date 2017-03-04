00ba632d3aeae439f15ddbca949a3bc9123e54d1
R̺̤͎͎̘̾̈́ͭ͠y̛̽͂̑̍d͈̥͔͉͇̳̍ͦ͛ͅe̲r̓̓ͤ͛̇͂ Ḋ̞͉͉͚͎̩̻̀̀̂ͤ͛o̭̝̗̬̩̗̍n̰̺̞̩̰ͪ̔͂̃̑͠a̞̽̃̉ͯͩ͟ȟ̥̱ͮu̠͌͞e̜̞̫ͦͧͤ̌ <Ryder Donahue>
2017-03-03 18:39:12 -0800

    PeerJS audio connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,69 @@ export class AppComponent {
   title = 'app works!';
   private count: number = 1;
   constructor() {
-    setInterval(() => {
-      this.count++;
-      if (this.count % 2 == 0) {
-        this.title += "\nno it doesnt!";
-      }else { 
-        this.title += "\nyes it does!";
-      }
-    }, 500);
+  }
+
+  @ViewChild('peerAudio') peerAudio: ElementRef;
+
+  peer;
+  anotherid;
+  mypeerid: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+
+  ngOnInit() {
+    this.peer = new Peer({ key: 's9hp9bp6p9cnmi' });
+    const self = this;
+    this.peer.on('open', function (id) {
+      self.mypeerid.next(id);
+    })
+
+    this.peer.on('connection', function (conn) {
+      conn.on('data', function (data) {
+        console.log(data);
+      });
+    });
+
+    const n = <any>navigator;
+
+    n.getUserMedia = (n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
+
+    this.peer.on('call', function (call) {
+
+      n.getUserMedia({ video: false, audio: true }, function (stream) {
+        call.answer(stream);
+        call.on('stream', function (remotestream) {
+        })
+      }, function (err) {
+        console.log('Failed to get stream', err);
+      })
+    })
+  }
+
+  connect() {
+    const conn = this.peer.connect(this.anotherid);
+    const self = this;
+    conn.on('open', function () {
+      conn.send('Message from that id');
+      self.audioConnect();
+    });
+  }
+
+  private audioConnect() {
+    const localvar = this.peer;
+    const fname = this.anotherid;
+
+    const n = <any>navigator;
+
+    n.getUserMedia = (n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
+    const self = this;
+
+    n.getUserMedia({ video: false, audio: true }, function (stream) {
+      const call = localvar.call(fname, stream);
+      call.on('stream', function (remotestream) {
+        self.peerAudio.nativeElement.src = (URL).createObjectURL(remotestream);
+      })
+    }, function (err) {
+      console.log('Failed to get stream', err);
+    })
   }
 }
--- src/graphicText/graphicText.component.ts
+++ src/graphicText/graphicText.component.ts
@@ -1,5 +1,5 @@
 import { OnInit, Component, Input, ChangeDetectorRef } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'graphic-text',
   templateUrl: './graphicText.component.html',
@@ -7,6 +7,7 @@ import { OnInit, Component, Input, ChangeDetectorRef } from '@angular/core';
 })
 export class GraphicText implements OnInit {
   @Input() textString: string;
+  @Input() textStringObs: BehaviorSubject<string>;
   @Input() speed: number;
   timer: NodeJS.Timer;
   public displayText: string;
@@ -20,62 +21,70 @@ export class GraphicText implements OnInit {
   ngOnInit() {
     this.displayText = '電';
     this.effectsText = ' ';
-    // this.timer = setInterval(() => {
-    //   if (this.displayText.length <= this.textString.length) {
-    //     // 電脳 計算機 電子計算機 端末 電算 電算機
-    //     //add correct character
-    //     // if (this.displayText.length > 0) {
-    //     this.effectsText = this.effectsText.slice(0, this.effectsText.length - 1);
-    //     if (this.displayText.length > 1) {
-    //       this.effectsText += this.displayText[this.displayText.length - 2];
-    //     }
-    //     this.effectsText += this.chars[this.displayText.length % this.chars.length];
-    //     // this.effectsText += this.textString[this.displayText.length];
 
-    //     this.displayText = this.displayText.slice(0, this.displayText.length - 1);
-    //     this.displayText += this.textString[this.displayText.length];
-    //     this.displayText += this.chars[this.displayText.length % this.chars.length];
-    //     // }
-    //     // add kanji 
-    //   } else {
-    //     this.displayText = this.displayText.slice(0, this.displayText.length - 1);
-    //     this.effectsText = this.displayText;
-    //     this.writing = false;
-    //     clearInterval(this.timer);
-    //   }
-
-    //   this.ref.detectChanges();
-    // }, 90);
+    if (this.textString) {
       this.DrawText();
-
-
-
+    } else if (this.textStringObs) {
+      this.textStringObs.subscribe(str => {
+        this.displayText = '電';
+        this.effectsText = ' ';
+        this.textString = str;
+        this.DrawText();
+      });
+    }
   }
 
-  private DrawText(){ 
+  private DrawText() {
     if (this.displayText.length <= this.textString.length) {
-        // 電脳 計算機 電子計算機 端末 電算 電算機
-        //add correct character
-        // if (this.displayText.length > 0) {
-        this.effectsText = this.effectsText.slice(0, this.effectsText.length - 1);
-        if (this.displayText.length > 1) {
-          this.effectsText += this.displayText[this.displayText.length - 2];
-        }
-        this.effectsText += this.chars[this.displayText.length % this.chars.length];
-        // this.effectsText += this.textString[this.displayText.length];
-
-        this.displayText = this.displayText.slice(0, this.displayText.length - 1);
-        this.displayText += this.textString[this.displayText.length];
-        this.displayText += this.chars[this.displayText.length % this.chars.length];
-        // }
-        // add kanji 
-        setTimeout(()=> { this.DrawText() }, Math.max(90 - this.displayText.length * this.speed * 3, 1));
-      } else {
-        this.displayText = this.displayText.slice(0, this.displayText.length - 1);
-        this.effectsText = this.displayText;
-        this.writing = false;
-        // clearInterval(this.timer);
+      // 電脳 計算機 電子計算機 端末 電算 電算機
+      //add correct character
+      // if (this.displayText.length > 0) {
+      this.effectsText = this.effectsText.slice(0, this.effectsText.length - 1);
+      if (this.displayText.length > 1) {
+        this.effectsText += this.displayText[this.displayText.length - 2];
       }
-      this.ref.detectChanges();
+      this.effectsText += this.chars[this.displayText.length % this.chars.length];
+      // this.effectsText += this.textString[this.displayText.length];
+
+      this.displayText = this.displayText.slice(0, this.displayText.length - 1);
+      this.displayText += this.textString[this.displayText.length];
+      this.displayText += this.chars[this.displayText.length % this.chars.length];
+      // }
+      // add kanji 
+      setTimeout(() => { this.DrawText() }, Math.max(90 - this.displayText.length * this.speed * 3, 1));
+    } else {
+      this.displayText = this.displayText.slice(0, this.displayText.length - 1);
+      this.effectsText = this.displayText;
+      this.writing = false;
+      // clearInterval(this.timer);
+    }
+    this.ref.detectChanges();
+  }
+
+  private DrawTextObs() {
+    if (this.displayText.length <= this.textStringObs.value.length) {
+      // 電脳 計算機 電子計算機 端末 電算 電算機
+      //add correct character
+      // if (this.displayText.length > 0) {
+      this.effectsText = this.effectsText.slice(0, this.effectsText.length - 1);
+      if (this.displayText.length > 1) {
+        this.effectsText += this.displayText[this.displayText.length - 2];
+      }
+      this.effectsText += this.chars[this.displayText.length % this.chars.length];
+      // this.effectsText += this.textString[this.displayText.length];
+
+      this.displayText = this.displayText.slice(0, this.displayText.length - 1);
+      this.displayText += this.textStringObs.value[this.displayText.length];
+      this.displayText += this.chars[this.displayText.length % this.chars.length];
+      // }
+      // add kanji 
+      setTimeout(() => { this.DrawText() }, Math.max(90 - this.displayText.length * this.speed * 3, 1));
+    } else {
+      this.displayText = this.displayText.slice(0, this.displayText.length - 1);
+      this.effectsText = this.displayText;
+      this.writing = false;
+      // clearInterval(this.timer);
+    }
+    this.ref.detectChanges();
   }
 }
